8e12d99b82cbe6c731808fdf6604fbcc14a1efb4
kanchanrika27-hub <kanchanrika27@gmail.com>
2026-08-26 21:08:29 +0700

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,12 @@
+export const products = [
+  { name: 'តែទឹកឃ្មុំល្មុង', desc: 'តែអាឡឺម៉ុងលាយជាមួយទឹកឃ្មុំធម្មជាតិ', price: 8000, img: '/images/honey-lemon.jpg', isNew: false },
+  { name: 'តែផ្កាម្រេច', desc: 'តែផ្កាម្រេចក្រអូបបែបធម្មជាតិ', price: 7000, img: '/images/jasmine-tea.jpg', isNew: false },
+  { name: 'តែបូបា', desc: 'តែទឹកដោះគោជាមួយគ្រាប់បូបា', price: 10000, img: '/images/boba-tea.jpg', isNew: false },
+  { name: 'តែផ្លែឈើ', desc: 'តែផ្លែឈើបន្លែបន្ទប់ថ្មីស្រស់', price: 9000, img: '/images/fruit-tea.jpg', isNew: false },
+  { name: 'តែបន្សំ', desc: 'តែបន្សំជួយដោះលែងជាតិពុល', price: 12000, img: '/images/herbal-detox.jpg', isNew: false },
+  { name: 'តែបៃតង', desc: 'តែបៃតងធម្មជាតិ សំរាប់សុខភាព', price: 6500, img: '/images/green-tea.jpg', isNew: true },
+  { name: 'តែទឹកដោះគោ', desc: 'តែទឹកដោះគោក្រអូប និងរីករម្យ', price: 8500, img: '/images/milk-tea.jpg', isNew: true },
+  { name: 'តែស្វាយ', desc: 'តែស្វាយស្រស់ មានរសជាតិឈើទឹក', price: 9500, img: '/images/mango-tea.jpg', isNew: true },
+  { name: 'ម៉ាឆាឡាតេ', desc: 'ម៉ាឆាជប៉ុនលាយទឹកដោះគោក្រអូប', price: 11000, img: '/images/matcha-latte.jpg', isNew: true },
+  { name: 'តែផ្កាកុលាប', desc: 'តែផ្កាកុលាបក្រអូប ជួយបន្សយអារម្មណ៍', price: 10500, img: '/images/rose-tea.jpg', isNew: true },
+]
